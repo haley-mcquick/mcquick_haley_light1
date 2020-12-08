14f46b085296b9e0379d2b87443470aa7706f335
haley-mcquick <72761290+haley-mcquick@users.noreply.github.com>
2020-12-08 11:02:15 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+while (true) {
+    console.log("Light level: " + input.lightLevel())
+    if (input.lightLevel() <= 40) {
+        light.setAll(color.rgb(255, 0, 0))
+    } else if (input.lightLevel() > 40) {
+        light.setAll(color.rgb(0, 255, 255))
+    }
+    
+}
